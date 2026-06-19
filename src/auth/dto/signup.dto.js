df2f3602fa4557b9282218092f2d42aebe1319d5
Rakshit Pandey <rakshitpandey0321@gmail.com>
2026-06-19 15:38:32 +0530

    Cleanup duplicate task module and generated JS files

--- src/auth/dto/signup.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.SignupDto = void 0;
-var SignupDto = /** @class */ (function () {
-    function SignupDto() {
-    }
-    return SignupDto;
-}());
-exports.SignupDto = SignupDto;
