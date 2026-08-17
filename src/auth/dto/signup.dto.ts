4c156314aad38ceafb0d49e6ce201360ead02ed0
Rakshit Pandey <rakshitpandey0321@gmail.com>
2026-08-17 22:51:40 +0530

    fix: validate auth request DTOs

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,6 +1,23 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
 export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
   name!: string;
+
+  @IsEmail()
   email!: string;
+
+  @IsString()
+  @MinLength(6)
   password!: string;
+
+  @IsString()
+  @IsNotEmpty()
   tenantName!: string;
 }
