03cff30075e79b05c0d37edbb544b4cb8f71adef
Rakshit Pandey <rakshitpandey0321@gmail.com>
2026-08-28 00:42:59 +0530

    feat: add task attachment entity and upload dependencies

---
+++ src/tasks/task-attachment.entity.ts
@@ -0,0 +1,43 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Task } from './task.entity';
+import { User } from '../users/user.entity';
+
+@Entity('task_attachments')
+export class TaskAttachment {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  originalName!: string;
+
+  @Column()
+  fileName!: string;
+
+  @Column()
+  filePath!: string;
+
+  @Column()
+  mimeType!: string;
+
+  @Column('bigint')
+  size!: number;
+
+  @ManyToOne(() => Task, {
+    onDelete: 'CASCADE',
+  })
+  task!: Task;
+
+  @ManyToOne(() => User, {
+    onDelete: 'CASCADE',
+  })
+  uploadedBy!: User;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
